e8649a2d284e061061f089e61d452e331067563d
Veysel Karani Turan <karanituran@hotmail.com>
2022-05-20 13:02:57 +0300

    Add Business layer concretes and abstracts

---
+++ KutuphaneOtomasyonu/Business/KitapService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KutuphaneOtomasyonu.Business
+{
+    internal interface KitapService
+    {
+    }
+}
